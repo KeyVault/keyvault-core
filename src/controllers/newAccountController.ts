52e6d49b79e9a3fcd92fda348a456298907f8bd7
Asadbek Karimov <asadbekasadbek@gmail.com>
2023-05-26 16:18:52 -0400

    endpoint /signup done

--- src/controllers/newAccountController.ts
+++ src/controllers/newAccountController.ts
@@ -16,20 +16,29 @@ const newAccountController =async (
     next: NextFunction) => {
     try {
         const { email, password } = req.body as Body;
-
-        const { data, error } =  await supabase.auth.signUp({
+        
+        // create a new user at supabase
+        const { data:signupData, error:signUpError } =  await supabase.auth.signUp({
             email,
             password
         });
     
-        if (error) {
-            throw new Api400Error(error.message);
+        if (signUpError) {
+            throw new Api400Error(signUpError.message);
         }
     
-        console.log('Account created successfully:', data);
+        // append to users table
+        const { data:appendUserData, error:appendUserError } = await supabase.from('users').insert([
+            { id: signupData.user?.id, email: signupData.user?.email, created_at: signupData.user?.created_at, updated_at: signupData.user?.updated_at },
+        ]);
+        
+        if (appendUserError) {
+            throw new Api400Error(appendUserError.message);
+        }
+        
         res.status(200).json({
             success: true,
-            msg: data
+            msg: appendUserData
         });
         
         } catch (error) {
